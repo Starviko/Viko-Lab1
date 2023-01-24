b02ea17eb839c316108a7370d48ade2d65793809
StarWalker-v <pengjingguo2022@gmail.com>
2023-01-24 14:50:49 -0700

    Add project files.

---
+++ Lab1/Person.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Lab1
+{
+    internal class Person
+    {
+        private int personId;
+        private string firstName;
+        private string lastName;
+        private int age;
+        private string favoriteColor;
+        private bool isWorking;
+
+        public Person(int personId, string firstName, string lastName, int age, string favoriteColor, bool isWorking)
+        {
+            this.personId = personId;
+            this.firstName = firstName;               
+            this.lastName = lastName;
+            this.age = age;               
+            this.favoriteColor = favoriteColor;
+            this.isWorking = isWorking;
+        }
+
+        public void DisplayPersonInfo()
+        {
+            int personId = this.personId;
+            string firstName = this.firstName;
+            string lastName = this.lastName;
+            string name = firstName +" "+ lastName;
+            string favoriteColor = this.favoriteColor;
+            Console.WriteLine(personId+ ": " + name+"'s favorite colour is "+favoriteColor);
+        }
+        
+        public string ChangeFavoriteColor()
+        {
+            this.favoriteColor = "White";
+            string color = this.favoriteColor;
+            return color;
+        }
+
+        public void GetAgeInTenYears()
+        {
+            string firstName = this.firstName;
+            string lastName = this.lastName;
+            string name = firstName +" " +lastName;
+            int age = this.age + 10;
+            Console.WriteLine(name + "'s Age in 10 years is: " + age);
+        }
+
+        public override string ToString()
+        {
+            int personId = this.personId;
+            string firstName = this.firstName;
+            string lastName = this.lastName;
+            int age = this.age;
+            string favoriteColor = this.favoriteColor;
+            bool isWorking = this.isWorking;
+            return "PersonId: " + personId + "\nFirstName: " + firstName + "\nLastName: " + lastName
+                + "\nFavoriteColor: " + favoriteColor + "\nAge: " + age
+                + "\nIsWorking: " + isWorking;
+        }
+
+    }
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1;
+            person1 = new Person(1,"Ian","Brooks", 30, "Red",true);
+
+            Person person2;
+            person2 = new Person(2, "Gina", "James", 18, "Green", false);
+            person2.DisplayPersonInfo();
+
+            Person person3;
+            person3 = new Person(3, "Mike", "Briscoe", 45, "Blue", true);
+            Console.WriteLine(person3);
+
+            person1.ChangeFavoriteColor();
+            person1.DisplayPersonInfo();
+            
+            Person person4;
+            person4 = new Person(4, "Mary", "Beals", 28, "Yellow", true);
+            person4.GetAgeInTenYears();
+
+            //make a Person list
+            List<Person> persons = new List<Person>();
+            persons.Add(person1);
+            persons.Add(person2);
+            persons.Add(person3);
+            persons.Add(person4);
+            
+            
+        }
+    }
+}
